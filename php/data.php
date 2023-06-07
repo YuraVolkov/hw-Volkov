919a6ebe9fbcf467226d1d16de059f0d07e7b62a
Yura Volkov <yura.volkov90@mail.ru>
2023-06-07 07:03:58 +0500

    7_06_dz

--- php/data.php
+++ php/data.php
@@ -1,7 +1,10 @@
 <?php
-$birthday = '23.05.1990';
+function dayCount($birthday){
 $today = date('d.m.y');
 echo "Дата рождения" . " " . $birthday . '<br>';
 echo "Сегодня" . " " . $today . '<br>';
 echo "Количество дней между датами" . " " . $date = date_diff(new DateTime($birthday), new DateTime($today))->days;
+}
+$birthday = '23.05.1990';
+dayCount($birthday);
 ?>
--- wordCount.php
+++ wordCount.php
@@ -1,5 +1,5 @@
 <?php
-$text_1 = strip_tags(file_get_contents('index.php'));
+/*$text_1 = strip_tags(file_get_contents('index.php'));
 $wordCountIndex = str_word_count($text_1, 0, "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя");
 $text_2 = strip_tags(file_get_contents('php/content.php'));
 $wordCountContent = str_word_count($text_2, 0, "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя");
@@ -8,8 +8,20 @@ $wordCountContainer = str_word_count($text_3, 0, "АаБбВвГгДдЕеЁёЖ
 $text_4 = strip_tags(file_get_contents('php/container_2.php'));
 $wordCountContainer_2 = str_word_count($text_4, 0, "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя");
 $wordCount = $wordCountIndex + $wordCountContent + $wordCountContainer + $wordCountContainer_2;
-echo "Количество слов на странице " . $wordCount . "<br>";
-$text = $text_1 . $text_2 . $text_3 . $text_4;
+echo "Количество слов на странице " . $wordCount . "<br>";*/
+
+function wordCounts($text){ 
+    $wordCount = str_word_count($text, 0, "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя");
+    echo "Количество слов на странице " . $wordCount . "<br>";
+}
+$text = strip_tags(file_get_contents('index.php'));
+wordCounts($text);
+
+//$text = $text_1 . $text_2 . $text_3 . $text_4;
+
+function letter_count($text){
 $str = preg_match_all('/[АаЕеЁёИиОоУуЭэЮюЯя]/u', $text);
-echo "Количество гласных на странице " . $str;
+echo "Количество гласных на странице " . $str;}
+$text = "Работаю на комбинате горновым, в доменном цехе, хочу перейти работать в IT, прошел несколько небольших курсов по верстке и работе в Figma";
+letter_count($text);
 ?>
